a34eb2295857aa4adeebf9500fb7bf72b3a088f6
Tiago Schallenberg <81322521+TiagoSchallenberg@users.noreply.github.com>
2022-06-17 10:30:19 -0300

    Update Product.js

--- scr/models/Product.js
+++ scr/models/Product.js
@@ -1,6 +1,6 @@
 const mysql = require('../config/connect.js')
 
-const productSchema = new mysql.Schema(             // Arrumar o tipo de dados
+const productSchema = new mysql.Schema(             
     {
         id: {type: String},
         nome: {type: String, require: true},
